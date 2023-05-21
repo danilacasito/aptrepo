67524e4ea50ae1c2524933edb21c5faa20414878
Daniel <danilacasito8@gmail.com>
2023-05-21 13:31:43 +0200

    update

---
+++ ubuntu/add_packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+EMAIL="danilacasito8@gmail.com"
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+
+apt-ftparchive release . > Release
+gpg --default-key "$EMAIL" --abs -o - Release > Release.gpg
+gpg --default-key "$EMAIL" --clearsign -o - Release > InRelease
+
+git add .
+git commit -m update
+git push
